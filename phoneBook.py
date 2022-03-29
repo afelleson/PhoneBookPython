675e212212dc149e8caa8b438839f5263d56b52e
afelleson <86610161+afelleson@users.noreply.github.com>
2022-03-28 23:33:23 -0400

    update comments

--- phoneBook.py
+++ phoneBook.py
@@ -18,11 +18,11 @@ class phoneBook:
     )
     return
 
-  def findByLast(self,last):
-    mycursor = self.mydb.cursor()
-    mycursor.execute("SELECT * FROM PhoneBook WHERE Last like '%"+last+"%'"); # SQL code that manipulates the table PhoneBook in your database
-    myresult = mycursor.fetchall()
-    return(myresult)
+  def findByLast(self,last): # function to search by last name
+    mycursor = self.mydb.cursor() # create(?) and name a cursor (bound to this connection; there can be more than one active at a time)
+    mycursor.execute("SELECT * FROM PhoneBook WHERE Last like '%"+last+"%'"); # SQL code that pull all rows from the table PhoneBook in your database
+    myresult = mycursor.fetchall() #Fetch all (remaining) rows of the above query result, returning them as a list of tuples. An empty list is returned if there is no more record to fetch. A ProgrammingError is raised if the previous call to execute*() did not produce any result set or no call was issued yet.
+    return(myresult) # Return all the rows fitting the condition as a list of tuples
 
   def findByFirst(self,first):
     mycursor = self.mydb.cursor()
@@ -41,10 +41,10 @@ class phoneBook:
       self.mydb.close()
     return ("success")
 
-  def addEntry(self,first,last,phone,ptype):
+  def addEntry(self,first,last,phone,ptype): # function to add an entry to the database table
     mycursor = self.mydb.cursor()
     try:
-      mycursor.execute("INSERT INTO PhoneBook(First,Last,Phone,Type) VALUES ('"+first+"','"+last+"','"+phone+"','"+ptype+"')")
+      mycursor.execute("INSERT INTO PhoneBook(First,Last,Phone,Type) VALUES ('"+first+"','"+last+"','"+phone+"','"+ptype+"')") # SQL code that manipulates the table PhoneBook in your database
       self.mydb.commit()
     except Exception as e:
       return "Error,"+ str(e)
